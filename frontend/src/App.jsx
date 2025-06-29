c29e25114d02ee05434f361414692c2d98bdde92
anandrarksali <anandrarkasali@gmail.com>
2025-06-30 00:14:14 +0530

    Initial project setup: Backend & Frontend scaffolding
    Initial setup complete: Set up React frontend and FastAPI backend with basic folder structure to start development.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,6 @@
+export default function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600">
+    </div>
+  );
+}
---
+++ frontend/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
